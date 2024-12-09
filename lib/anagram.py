0807411bfc617425083ca5b6dffc1c8a0bfb7f35
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-12-09 19:34:47 +0300

    Completed anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__ (self, word):
+        self.word = word
+    
+    def match(self, words):
+        return [sim_word for sim_word in words if sorted(sim_word) == sorted(self.word)]
+
+        # for sim_word in words:
+        #     if sorted(sim_word) == sorted(self.word):
+        #         return sim_word
+
+
+
+anagram = Anagram('liten')
+print(anagram.match(['enlists', 'google', 'inlets', 'banana']))
+
